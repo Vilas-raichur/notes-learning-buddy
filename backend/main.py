c8bbfd2a0132ab93814e5a0e289c3438fc52b62c
Vilas Raichur <vilasraichur@gmail.com>
2026-09-07 14:29:52 +0530

    add /me protected endpoint using JWT token verification

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from fastapi.security import OAuth2PasswordRequestForm
-from security import verify_password, create_access_token
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 
 from database import get_db
 import models
 import schemas
-from security import hash_password
+from security import hash_password, verify_password, create_access_token, decode_access_token
 
 app = FastAPI()
 
@@ -29,7 +28,6 @@ def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-
 @app.post("/login")
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == form_data.username).first()
@@ -38,3 +36,20 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depend
 
     access_token = create_access_token(data={"sub": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    payload = decode_access_token(token)
+    if payload is None:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+    email = payload.get("sub")
+    user = db.query(models.User).filter(models.User.email == email).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+    return user
+
+@app.get("/me", response_model=schemas.UserResponse)
+def read_current_user(current_user: models.User = Depends(get_current_user)):
+    return current_user
